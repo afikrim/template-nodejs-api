832738a3a1fd21e1cdea63481ab2c7102d7ac27f
Aziz Fikri <afikrim10@student.ub.ac.id>
2020-01-31 13:46:56 +0700

    Setup environment

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'no-restricted-globals': 'off',
+    'import/no-unresolved': 'off'
+  }
+}
